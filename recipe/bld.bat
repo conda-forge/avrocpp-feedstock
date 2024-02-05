8fe97f9fcd679551fc8fd2a2e73befb2529a5b0c
Daniel Nugent <nugend@Daniels-MacBook-Pro.local>
2024-02-05 02:10:34 -0500

    Update package for upsteam sources change. Enforce C++ 14 compatiblitiy.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,6 +10,10 @@ cmake -G "NMake Makefiles" ^
     -DSNAPPY_ROOT_DIR:PATH="%PREFIX%" ^
     -DCMAKE_BUILD_TYPE:STRING=Release ^
     -DBUILD_SHARED_LIBS=ON ^
+    -DCMAKE_CXX_FLAGS="-Wno-error=c++20-compat" ^
+    -DCMAKE_CXX_STANDARD=14 ^
+    -DCMAKE_CXX_STANDARD_REQUIRED=ON ^
+    -DCMAKE_CXX_EXTENSIONS=OFF ^
     ..
 if errorlevel 1 exit 1
 
