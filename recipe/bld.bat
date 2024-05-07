cb4f63894e40fa2a417789310c61c8f8b92894c5
Austin Morton <austin.morton@aquatic.com>
2024-05-07 20:11:56 +0000

    Upgrade to 1.11.3

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-mkdir build
-cd build
-
-cmake -G "NMake Makefiles" ^
-    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+cmake %CMAKE_ARGS% -G "NMake Makefiles" ^
+    -S lang/c++ ^
+    -B build ^
     -DBOOST_ROOT:PATH="%PREFIX%" ^
     -DSNAPPY_ROOT_DIR:PATH="%PREFIX%" ^
     -DCMAKE_BUILD_TYPE:STRING=Release ^
     -DBUILD_SHARED_LIBS=ON ^
-    ..
+    -DCMAKE_CXX_STANDARD=14 ^
+    -DCMAKE_CXX_STANDARD_REQUIRED=ON ^
+    -DCMAKE_CXX_EXTENSIONS=OFF ^
+
 if errorlevel 1 exit 1
 
+cd build
 nmake
 if errorlevel 1 exit 1
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-mkdir build
-cd build
-echo $PREFIX
+set -euxo pipefail
 
-cmake .. \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+cmake \
+  ${CMAKE_ARGS} \
+  -S lang/c++ \
+  -B build \
   -DBOOST_ROOT=$PREFIX \
   -DSNAPPY_ROOT_DIR=$PREFIX \
-  -DCMAKE_BUILD_TYPE=RelWithDebInfo 
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DCMAKE_CXX_STANDARD=14 \
+  -DCMAKE_CXX_STANDARD_REQUIRED=ON \
+  -DCMAKE_CXX_EXTENSIONS=OFF \
 
-make # VERBOSE=1
+cd build
+make
 make test
 make install
