421f22030e87186c2a97301f3506e70c6ed0e699
Austin Morton <austin.morton@aquatic.com>
2024-05-08 12:47:53 +0000

    Use ninja for verbose compile commands on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-cmake %CMAKE_ARGS% -G "NMake Makefiles" ^
+cmake ^
+    %CMAKE_ARGS% ^
     -S lang/c++ ^
     -B build ^
+    -G Ninja ^
     -DBOOST_ROOT:PATH="%PREFIX%" ^
     -DSNAPPY_ROOT_DIR:PATH="%PREFIX%" ^
     -DCMAKE_BUILD_TYPE:STRING=Release ^
     -DBUILD_SHARED_LIBS=ON ^
     -DCMAKE_CXX_STANDARD=14 ^
     -DCMAKE_CXX_STANDARD_REQUIRED=ON ^
-    -DCMAKE_CXX_EXTENSIONS=ON ^
-
+    -DCMAKE_CXX_EXTENSIONS=OFF ^
+    -DCMAKE_VERBOSE_MAKEFILE=ON
 if errorlevel 1 exit 1
 
-cd build
-nmake
+cmake --build build --config Release --target test
 if errorlevel 1 exit 1
 
-nmake install
+cmake --build build --config Release --target install
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -6,14 +6,15 @@ cmake \
   ${CMAKE_ARGS} \
   -S lang/c++ \
   -B build \
+  -G Ninja \
   -DBOOST_ROOT=$PREFIX \
   -DSNAPPY_ROOT_DIR=$PREFIX \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DBUILD_SHARED_LIBS=ON \
   -DCMAKE_CXX_STANDARD=14 \
   -DCMAKE_CXX_STANDARD_REQUIRED=ON \
   -DCMAKE_CXX_EXTENSIONS=OFF \
+  -DCMAKE_VERBOSE_MAKEFILE=ON
 
-cd build
-make
-make test
-make install
+cmake --build build --config RelWithDebInfo --target test
+cmake --build build --config RelWithDebInfo --target install
