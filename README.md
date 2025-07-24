86af19da708969935cfefac913142a1bba2455df
Austin Morton <austin.morton@aquatic.com>
2025-07-24 13:45:12 -0500

    fix osx and rattlerize

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ About avrocpp-feedstock
 
 Feedstock license: [BSD-3-Clause](https://github.com/conda-forge/avrocpp-feedstock/blob/main/LICENSE.txt)
 
-Home: https://avro.apache.org
+Home: https://avro.apache.org/
 
 Package license: Apache-2.0
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,11 @@
 
 set -euxo pipefail
 
+if [[ "${target_platform}" == osx-* ]]; then
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 cmake \
   ${CMAKE_ARGS} \
   -S lang/c++ \
