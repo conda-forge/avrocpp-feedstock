be801b045ca999aa20743d502826adeb9c62495e
Austin Morton <apmorton@users.noreply.github.com>
2025-07-25 13:27:08 -0500

    avrocpp 1.12.0 and boost 1.88 (#33)

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,11 @@
 
 set -euxo pipefail
 
+if [[ "${target_platform}" == osx-* ]]; then
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 cmake \
   ${CMAKE_ARGS} \
   -S lang/c++ \
@@ -11,7 +16,7 @@ cmake \
   -DSNAPPY_ROOT_DIR=$PREFIX \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DBUILD_SHARED_LIBS=ON \
-  -DCMAKE_CXX_STANDARD=14 \
+  -DCMAKE_CXX_STANDARD=17 \
   -DCMAKE_CXX_STANDARD_REQUIRED=ON \
   -DCMAKE_CXX_EXTENSIONS=OFF \
   -DCMAKE_VERBOSE_MAKEFILE=ON
